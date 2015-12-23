aaab910980b0e8d83d5f8c193c2675cc70592f3f
Oscar Swanros <oscar@swanros.com>
2015-12-23 17:48:59 -0600

    Add extension on UIApplication to work as appearance proxy

---
+++ PUIKit/Extensions/UIApplication.swift
@@ -0,0 +1,19 @@
+
+extension UIApplication {
+    public func setPrimaryColor(
+        primary: Color,
+        secondaryColor secondary: Color,
+        terciaryColor terciary: Color) {
+            UINavigationBar.appearance().barTintColor = primary
+            UINavigationBar.appearance().tintColor = secondary
+            
+            UIBarButtonItem.appearance().tintColor = secondary
+            
+            UITabBar.appearance().barTintColor = secondary
+            UITabBar.appearance().tintColor = primary
+            
+            UINavigationBar.appearance().titleTextAttributes = [
+                NSForegroundColorAttributeName: secondary
+            ]
+    }
+}
