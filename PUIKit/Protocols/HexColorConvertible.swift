339cd2d816b3f96bcca5d86019de22a7202c9886
Oscar Swanros <oscar@swanros.com>
2015-12-26 14:27:44 -0600

    Make HexColorConvertible Hashable and Equatable

--- PUIKit/Protocols/HexColorConvertible.swift
+++ PUIKit/Protocols/HexColorConvertible.swift
@@ -1,4 +1,14 @@
 
-public protocol HexColorConvertible {
+public protocol HexColorConvertible: Hashable {
     func hexColor() -> String
+}
+
+extension HexColorConvertible {
+    public var hashValue: Int {
+        return hexColor().hashValue
+    }
+}
+
+public func ==<C: HexColorConvertible>(lhs: C, rhs: C) -> Bool {
+    return lhs.hashValue == rhs.hashValue
 }
