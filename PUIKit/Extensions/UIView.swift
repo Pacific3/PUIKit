7b6c14468999a8660ba5a97ffb3f964c8eee7485
Oscar Swanros <oscar@swanros.com>
2015-12-28 11:29:39 -0600

    Add method to set border color

--- PUIKit/Extensions/UIView.swift
+++ PUIKit/Extensions/UIView.swift
@@ -5,4 +5,8 @@ extension View {
     public func p_setBackgroundColor<C: ColorConvertible>(color: C) {
         backgroundColor = color.color()
     }
+    
+    public func p_setBorderColor<C: ColorConvertible>(color: C) {
+        layer.borderColor = color.color().CGColor
+    }
 }
